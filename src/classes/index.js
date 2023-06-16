c3fad2b3c3c2f6eb371411c2b117902052fe369c
Nanapiou <nana.juniot@gmail.com>
2023-06-16 21:51:07 +0200

    Adding bitfield in export

--- src/classes/index.js
+++ src/classes/index.js
@@ -4,8 +4,9 @@ import Client from './Client.js';
 import Rest from './Rest.js';
 import WebSocketShard from './WebSocketShard.js';
 import WebSocketManager from './WebSocketManager.js';
+import BitField from './BitField.js';
 
 export {
-    Api, Client, Rest, WebSocketManager, WebSocketShard, Cache
+    Api, Client, Rest, WebSocketManager, WebSocketShard, Cache, BitField
 };
 
